c28dd43aff6f3c64d0c2f3066683044345ebd5d6
Edgar Marroquin <emarro@cs.cornell.edu>
2025-03-06 13:12:12 -0500

    Fix formatting in var names

--- examples/msa/training.sh
+++ examples/msa/training.sh
@@ -2,31 +2,31 @@
 # Data sources and output 
 # see README for how to download and unzip MSA:
 # https://huggingface.co/datasets/songlab/multiz100way
-msa_path = "/share/kuleshov/emm392/lrb_benchmark_boilerplate/gpn_data/89.zarr"
-training_windows_path = "songlab/gpn-msa-sapiens-dataset"
-output_path = "checkpoints"  # TODO: might need to do mkdir
+msa_path="/share/kuleshov/emm392/lrb_benchmark_boilerplate/gpn_data/89.zarr"
+training_windows_path="songlab/gpn-msa-sapiens-dataset"
+output_path="checkpoints"  # TODO: might need to do mkdir
 mkdir -p $output_path
 
 # Hyperparameters
-max_steps = 10 # just for demonstration, should be 30_000 in a real run
-loss_weight = 0.1
-seed = 42
-use_aux_features = True
-weight_conserved = True
-flip_nonconserved = True
-n_aux_features = $((89 * 5)) # (n_species * #{A,C,G,T,-})
-config_overrides = f"n_aux_features=${n_aux_features}"  # here you can add e.g. ,hum_hidden_layers=8
+max_steps=10 # just for demonstration, should be 30_000 in a real run
+loss_weight=0.1
+seed=42
+use_aux_features=True
+weight_conserved=True
+flip_nonconserved=True
+n_aux_features=$((89 * 5)) # (n_species * #{A,C,G,T,-})
+config_overrides="n_aux_features=${n_aux_features}"  # here you can add e.g. ,hum_hidden_layers=8
 
 # System-specific config
 # The recommended total batch size is 2048
 # Since I'm running this notebook with 1 GPU, I'll put per_device_batch_size=512
 # and gradient_accumulation_steps=4
-n_gpu = 1
-per_device_batch_size = 512 # whatever fits in your GPU
-gradient_accumulation_steps = 4
-dataloader_num_workers = 8  # number of CPUs
-torchrun_path = "torchrun"  # might just be "torchrun" in your system
-report_to = "wandb"  # we usually use wandb (might need to create an account)
+n_gpu=1
+per_device_batch_size=512 # whatever fits in your GPU
+gradient_accumulation_steps=4
+dataloader_num_workers=8  # number of CPUs
+torchrun_path="torchrun"  # might just be "torchrun" in your system
+report_to="wandb"  # we usually use wandb (might need to create an account)
 WANDB_PROJECT=GPN_MSA_SAPIENS_EXAMPLE {torchrun_path} --nproc_per_node={n_gpu} -m gpn.msa.train --do_train \
     --do_eval --fp16 --report_to {report_to} --prediction_loss_only True \
     --dataset_name {training_windows_path} \
